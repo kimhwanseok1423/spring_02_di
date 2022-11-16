5022552373804326dc2ab51bfaf6686f31a8ef3d
kimhwanseok <kcsss4499@gmail.com>
2022-11-16 19:34:18 +0900

    new

---
+++ src/main/java/part01/MessageBeanEnglish.java
@@ -0,0 +1,15 @@
+
+package part01;
+
+public class MessageBeanEnglish implements MessageBean{
+public MessageBeanEnglish() {
+	System.out.println("MessageBeanEnglish");
+}
+
+@Override
+public void sayHello(String name) {
+	System.out.printf("Hello,%s !!!",name);
+	
+}
+
+}
